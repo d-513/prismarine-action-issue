5590a906b62973ed8dab53c32ab1feb9abe419c9
dada513 <kontakt@dada513.eu>
2020-11-15 10:22:27 +0100

    hmm

--- action.js
+++ action.js
@@ -10,11 +10,7 @@ async function app() {
   const octokit = new Octokit({
     authStrategy: createActionAuth,
   });
-  console.log(event.body);
-  if (!event.body) {
-    event.body = "Empty";
-  }
-  if (event.body.includes("--is-template")) {
+  if (event.issue.body.includes("--is-template")) {
     octokit.issues.createComment({
       owner: event.repository.owner.login,
       repo: event.repository.name,
