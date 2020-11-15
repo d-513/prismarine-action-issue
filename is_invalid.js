82b5f34ad68d972130819798608aae0ecf10726d
dada513 <kontakt@dada513.eu>
2020-11-15 14:45:26 +0100

    improve error message

--- is_invalid.js
+++ is_invalid.js
@@ -16,7 +16,7 @@ module.exports = (owner, repo, number) => {
       "Error: You did not fill up the issue template.",
 
       "You should be able to choose one when creating your issue. See image below.",
-      "If you edited your issue to the correct template, recheck the issue status by using p!recheck.",
+      "If you edited your issue to the correct template, recheck the issue status by commenting `p!recheck`.",
       "![Choosing the template - Example](https://i.imgur.com/z4pqWWZ.png)",
       "*If you belive this is an error, please reopen this issue.*",
     ].join("\n"),
