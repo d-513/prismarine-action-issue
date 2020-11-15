50bf98154653b8b627da706a8c59e06070cc39f1
dada513 <kontakt@dada513.eu>
2020-11-15 16:59:57 +0100

    Final touches

--- is_valid.js
+++ is_valid.js
@@ -29,6 +29,13 @@ module.exports = async (owner, repo, number) => {
     name: "invalid",
   });
 
+  await octokit.reactions.createForIssue({
+    owner,
+    repo,
+    issue_number: number,
+    content: "rocket",
+  });
+
   await octokit.issues.update({
     owner,
     repo,
