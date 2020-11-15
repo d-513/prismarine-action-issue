9ca4391495a28bdc53390ce92c1e6138427f2bc9
Octocat <octocat@github.com>
2020-11-15 16:00:34 +0000

    Builder

--- build/index.js
+++ build/index.js
@@ -135,6 +135,13 @@ module.exports = async (owner, repo, number) => {
     name: "invalid",
   });
 
+  await octokit.reactions.createForIssue({
+    owner,
+    repo,
+    issue_number: number,
+    content: "rocket",
+  });
+
   await octokit.issues.update({
     owner,
     repo,
